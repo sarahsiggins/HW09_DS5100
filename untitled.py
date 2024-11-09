8af356658325018bad5506ad355b7f46e0a9fd8f
Sarah_Christen <sachristen@gmail.com>
2024-11-09 15:46:29 -0500

    update

--- untitled.py
+++
@@ -1,6 +0,0 @@
-import booklover
-import pandas as pd
-
-my_book_list = pd.DataFrame({'book_name':['Jane Eyre','Fight Club','The Divine Comedy','The Popol Vuh']
-                          ,'book_rating':[4,3,5,5]})
-book_test = BookLover('Sarah','sc8rg@vt.edu','fiction',4,my_book_list)
