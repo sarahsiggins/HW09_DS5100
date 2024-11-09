36002ae3fa3fe2808221146011cbef6895a11488
Sarah_Christen <sachristen@gmail.com>
2024-11-09 15:52:59 -0500

    name udpates

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='hw09',
+    name='booklover',
     version='0.1',
     description='A package to create a class of various book information.',
     url='https://github.com/sarahsiggins/HW09_DS5100',
     author='Sarah Christen',
     author_email='sc8rg@virginia.edu',
     license='MIT',
-    packages=['bookloverpkg']
+    packages=['booklover']
 )
